e0f05e86355e875b4183407d55ad344ec174ea1b
Kristian Veggeland <kristianov2000@hotmail.com>
2022-11-05 18:37:56 +0100

    More shit to testing

--- Test.py
+++ Test.py
@@ -1,7 +1,9 @@
 from Functions import *
 
+
+# ********************************************************************** Testing function with small program ***************************************************************
 def whatDoYoWant():
-    print("What do you whant?" + "\n")
+    print("What do you want?" + "\n")
     answer  = getNewAnswer()
 
     while int(answer) != 5:
@@ -12,11 +14,36 @@ def whatDoYoWant():
             launchNewFile()
             answer  = getNewAnswer()
         
-    print("Program terminated, off toy go now!")        
-    
+        
+    print("Program terminated, off you go now!") 
+
+
+# ************************************************************************ Supporting function for testing *********************************************************************    
 def getNewAnswer():
     answer = input("press 1 for creating list and 2 if you want to delete one" + "\n" "and 5 if you wish to terminate ")
+    check = checkIfAnswerIsLegal(answer)
+    print(check)
+    holder = 0
+    while holder == 0:
+        if check == 0:
+            answer = input("press 1 for creating list and 2 if you want to delete one" + "\n" "and 5 if you wish to terminate ")
+            check = checkIfAnswerIsLegal(answer)
+        elif check == 1:
+            holder = 1
+        
+
     return answer
+
+
+def checkIfAnswerIsLegal(answer):
+    oba = 0
+    if answer.isdigit(): 
+        oba = 1
+        return oba
+    else:
+        oba = 0
+        return oba
+    
 
 
 whatDoYoWant()
