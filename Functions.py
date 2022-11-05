5284f593e0607f4a22fff22cc7e8f04c75375b1e
Kristian Veggeland <kristianov2000@hotmail.com>
2022-11-05 12:29:45 +0100

    Adding confirm list, testing create list function

--- Functions.py
+++ Functions.py
@@ -1,2 +1,23 @@
+import random
 
-def createList():
+def confirmList():
+    
+    confirm = input("Do you want to create list: ")
+
+    if confirm == " ":
+        print("try again please")
+        confirm = input("Do you want to create list: ")
+
+    elif confirm == "y":
+        createList()
+    
+    elif confirm == "n":
+        print("okay, youy can fuck off then.")
+
+    
+    
+def createList(): 
+    print("ok!")  
+
+
+confirmList()
