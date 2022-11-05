4ba1c276d58f30ef5b80c7dd2ea7b26963c31674
Kristian Veggeland <kristianov2000@hotmail.com>
2022-11-05 18:06:38 +0100

    new shit

--- Test.py
+++ Test.py
@@ -1,3 +1,22 @@
 from Functions import *
 
-launch()
+def whatDoYoWant():
+    print("What do you whant?" + "\n")
+    answer  = getNewAnswer()
+
+    while int(answer) != 5:
+        if int(answer) == 1:
+            launchNewFile()
+            answer  = getNewAnswer()
+        elif int(answer) == 2:
+            launchNewFile()
+            answer  = getNewAnswer()
+        
+    print("Program terminated, off toy go now!")        
+    
+def getNewAnswer():
+    answer = input("press 1 for creating list and 2 if you want to delete one" + "\n" "and 5 if you wish to terminate ")
+    return answer
+
+
+whatDoYoWant()
